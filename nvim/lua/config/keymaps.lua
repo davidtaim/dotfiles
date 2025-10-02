d7b101726a08acdac9771090183f45b58d4f944d
David Chávez Rodríguez <davidtaim@hotmail.com>
2025-10-02 13:32:50 -0600

    feat: added keymap to restart highlight if not working fine (colorize the file)

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,1 +1,7 @@
-vim.keymap.set("n", "=", "<cmd>ColorizerToggle<CR>", {desc="Attach colorizer to buffer"})
+vim.keymap.set("n", "=", "<cmd>ColorizerToggle<CR>", { desc = "Attach colorizer to buffer" })
+vim.keymap.set(
+	"n",
+	"<leader>rh",
+	"<cmd>write | edit | TSBufEnable highlight<CR>",
+	{ desc = "Restore highlight if not working fine." }
+)
